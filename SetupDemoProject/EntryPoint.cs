a13fdb8feb41a7b862b05961eb200e9b8d6ce3ae
Dakshayani Chebrolu <dacheb@nuuday.dk>
2021-05-16 20:04:11 +0200

    Add project files.

---
+++ SetupDemoProject/EntryPoint.cs
@@ -0,0 +1,106 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Globalization;
+
+
+class EntryPoint
+{
+    static IWebDriver driver = null;
+    static IWebElement webElement = null;
+
+
+    static void Main()
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        String url = "https://www.amazon.com/";
+        String actualPrice = "";
+        String expectedPrice = "$100";
+
+        ChromeOptions options = new ChromeOptions();
+        options.AddArguments("--incognito");
+
+        //initialize the chrome driver 
+        driver = new ChromeDriver(options);
+        driver.Manage().Window.Maximize();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+
+        try
+        {
+           
+            //Navigate to the url
+            driver.Navigate().GoToUrl(url);
+            WaitForPageLoad(driver, wait);
+
+            //get the elements using ID and Xpath
+            driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys("laptop");
+
+            driver.FindElement(By.Id("nav-search-submit-button")).Click();
+
+            driver.FindElement(By.XPath("//div[@data-cel-widget='search_result_0']/descendant::h2/a[contains(@class,'a-text-normal')]")).Click();
+
+            WaitForPageLoad(driver, wait);
+
+            //Check whether the first selected item is on sale or not
+            webElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable((By.Id("priceblock_ourprice"))));
+            if(webElement.Enabled)    
+                actualPrice = driver.FindElement(By.Id("priceblock_ourprice")).Text;
+            else
+                actualPrice = driver.FindElement(By.Id("priceblock_saleprice")).Text;
+
+            decimal actualLaptopPrice = Decimal.Parse(actualPrice, NumberStyles.Number | NumberStyles.AllowCurrencySymbol, new CultureInfo("en-US"));
+
+            decimal expectedLaptopPrice = Decimal.Parse(expectedPrice, NumberStyles.Number | NumberStyles.AllowCurrencySymbol, new CultureInfo("en-US"));
+
+            SuccessMessage("Actual Price:" + actualLaptopPrice);
+
+            //Assertion for the actual price of the selected item with the expected price
+            Assert.Greater(actualLaptopPrice, expectedLaptopPrice, "Assertion was not successfull");
+
+            SuccessMessage("Test Passed");
+
+        }
+
+        catch (Exception e)
+        {
+            FailureMessage("Exception: " + e.Message);
+        }
+
+        finally
+        {
+            driver.Quit();
+            SuccessMessage("Browser closed");
+        }
+        watch.Stop();
+        
+        SuccessMessage($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+    }
+
+    //Page load mechanism to wait for the page to be loaded using explicit wait
+    public static void WaitForPageLoad(IWebDriver driver, WebDriverWait wait)
+    {
+        wait.Until(driver1 => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+    }
+
+    //Success message code for readability of messages
+    public static void SuccessMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    //Failure message code for readability of messages
+    public static void FailureMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+}
+
